c1b6327aa930ed7833ee0bf2a2298f3b0d31fd80
ana_rchy <hmmmtesthmmm2@gmail.com>
2024-05-13 14:41:16 +0100

    gifs now work, paths fixed so fact_check works when ran from both source or docker

--- src/commands.rs
+++ src/commands.rs
@@ -7,9 +7,9 @@ type Context<'a> = poise::Context<'a, Data, Error>;
 
 #[poise::command(prefix_command, slash_command)]
 pub async fn fact_check(ctx: Context<'_>) -> Result<(), Error> {
-    let image = get_fact_check_image().await;
+    let (image, filename) = get_fact_check_image().await;
     let attachment = CreateAttachment
-        ::file(&image, "fact_check.png").await.unwrap();
+        ::file(&image, filename).await.unwrap();
 
     let reply = CreateReply {
         attachments: vec![attachment],
@@ -22,12 +22,12 @@ pub async fn fact_check(ctx: Context<'_>) -> Result<(), Error> {
 }
 
 
-async fn get_fact_check_image() -> File {
-    let mut exec_path = std::env::current_exe().unwrap();
-    exec_path.pop();
+async fn get_fact_check_image() -> (File, String) {
+    let root_folder = get_assets_root_path();
 
-    let paths = std::fs::read_dir(format!("{}/assets/fact_check/", exec_path.display())).unwrap();
+    let paths = std::fs::read_dir(format!("{}/assets/fact_check/", root_folder)).unwrap();
     let mut images: Vec<String> = vec![];
+
     for path in paths {
         images.push(format!("{}", path.unwrap().path().display().to_string()));
     }
@@ -35,5 +35,15 @@ async fn get_fact_check_image() -> File {
     let rand_index = rand::random::<usize>() % images.len();
     let image = &images[rand_index];
 
-    File::open(image).await.unwrap()
+    (File::open(image).await.unwrap(), image.to_string())
+}
+
+fn get_assets_root_path() -> String {
+    let mut exec_path = std::env::current_exe().unwrap();
+    exec_path.pop();
+    if cfg!(debug_assertions) {
+        exec_path.pop(); exec_path.pop();
+    }
+
+    exec_path.to_string_lossy().to_string()
 }

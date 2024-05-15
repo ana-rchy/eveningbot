ed4ea7004cd4019c4be10c7d78fc0e4680dd41a0
ana_rchy <hmmmtesthmmm2@gmail.com>
2024-05-15 02:49:47 +0100

    tracks server leavings now

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use eveningbot::{commands, jobs};
-use poise::serenity_prelude::{self as serenity};
+use poise::serenity_prelude::{self as serenity, CreateMessage, GuildRef, GatewayIntents};
 use std::sync::Arc;
 use tokio_cron_scheduler::JobScheduler;
 
@@ -20,7 +20,7 @@ async fn main() -> Result<(), Error> {
 
 pub async fn poise_setup() -> serenity::Client {
     let token = std::env::var("DISCORD_TOKEN").expect("envvar the DISCORD_TOKEN");
-    let intents = serenity::GatewayIntents::non_privileged();
+    let intents =  GatewayIntents::GUILDS | GatewayIntents::GUILD_MEMBERS | GatewayIntents::MESSAGE_CONTENT;
 
     let framework = poise::Framework::<(), Error>::builder()
         .options(poise::FrameworkOptions {
@@ -28,6 +28,9 @@ pub async fn poise_setup() -> serenity::Client {
             prefix_options: poise::PrefixFrameworkOptions {
                 prefix: Some("!".into()),
                 ..Default::default()
+            },
+            event_handler: |ctx, event, framework, _| {
+                Box::pin(event_handler(ctx, event, framework))
             },
             ..Default::default()
         })
@@ -43,4 +46,35 @@ pub async fn poise_setup() -> serenity::Client {
         .framework(framework)
         .await
         .unwrap()
+}
+
+async fn event_handler(
+    ctx: &serenity::Context,
+    event: &serenity::FullEvent,
+    _framework: poise::FrameworkContext<'_, (), Error>
+) -> Result<(), Error> {
+    const USERS_REMOVED_CHANNEL_ID: u64 = 1240091460217344100;
+    let channel = serenity::ChannelId::new(USERS_REMOVED_CHANNEL_ID);
+
+    match event {
+        serenity::FullEvent::GuildMemberRemoval { guild_id, user, .. } => {
+            let member_count = {
+                let guild: GuildRef = ctx.cache.guild(guild_id).unwrap();
+                guild.member_count
+            };
+
+            let message = CreateMessage::new()
+                .content(
+                    format!("<@{}> left, now {} server members",
+                        user.id,
+                        member_count)
+                    );
+
+            let _ = channel.send_message(&ctx.http, message).await;
+        }
+
+        _ => {}
+    }
+
+    Ok(())
 }

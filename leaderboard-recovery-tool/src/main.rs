78895fb800f36838d7e03a6dc736627ea306cc15
ana_rchy <dicks@inmy.ass>
2024-05-31 02:26:10 +0100

    added leaderboard recovery tool

---
+++ leaderboard-recovery-tool/src/main.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+use std::io;
+use std::fs;
+use rmp_serde::encode;
+
+fn main() {
+    let mut leaderboard = HashMap::<u64, u16>::new();
+
+    println!("enter entries in format [user_id],[count]");
+    loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("cant read input");
+
+        let mut params = input.split(",");
+        let id = params.next().unwrap().parse::<u64>().unwrap();
+        let count = params.next().unwrap().parse::<u16>().unwrap();
+        leaderboard.insert(id, count);
+
+        let leaderboard_bytes = encode::to_vec(&leaderboard).expect("couldnt serialize leaderboard");
+        _ = fs::write("leaderboard.bin", leaderboard_bytes);
+    }
+}

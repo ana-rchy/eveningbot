55a88ba7285af3563d06460da2dd5ba580b2648c
ana_rchy <hmmmtesthmmm2@gmail.com>
2025-04-01 03:31:32 +0100

    april foos

--- src/jobs.rs
+++ src/jobs.rs
@@ -1,22 +1,22 @@
 use crate::global::*;
 use crate::web;
-use std::sync::{Arc, Mutex, atomic::Ordering};
+use log::debug;
 use poise::serenity_prelude::{self as serenity, Colour, CreateEmbed, CreateMessage, Http, UserId};
+use std::sync::{atomic::Ordering, Arc, Mutex};
 use tokio_cron_scheduler::{JobBuilder, JobScheduler, JobSchedulerError};
 use uuid::Uuid;
-use log::debug;
 
 pub async fn init_jobs(
     sched: Arc<JobScheduler>,
     client: &serenity::Client,
-    shared_data: &SharedData
+    shared_data: &SharedData,
 ) -> Result<(), JobSchedulerError> {
     #[allow(dead_code)]
     const TESTING_CHANNEL_ID: u64 = 1235087573421133824;
     const GENERAL_CHANNEL_ID: u64 = 1215048710074011692;
 
     let http = client.http.clone();
-    
+
     // arc my beloved
     let sunset_job_id: Arc<Mutex<Uuid>> = Default::default();
     let evening_bag = Arc::new(Mutex::new(EVENING_MOTD.to_vec()));
@@ -27,73 +27,16 @@ pub async fn init_jobs(
         // braces to ensure lock gets dropped
         let sunset_time = { *shared_data.sunset_time.lock().unwrap() };
 
-        let job = create_sunset_job(http.clone(), GENERAL_CHANNEL_ID, sunset_time, evening_bag.clone()).await;
-        { *sunset_job_id.lock().unwrap() = sched.add(job).await?; }
-    }
-
-    // show leaderboard - 12am
-    {
-        let http = http.clone();
-        let channel = serenity::ChannelId::new(GENERAL_CHANNEL_ID);
-        let leaderboard = shared_data.evening_leaderboard.clone();
-        let first_ge_sent = shared_data.first_ge_sent.clone();
-
-        let job = JobBuilder::new()
-            .with_timezone(chrono_tz::Europe::Dublin)
-            .with_cron_job_type()
-            .with_schedule("0 0 0 * * *")
-            .unwrap()
-            .with_run_async(Box::new(move |_uuid, _l| {
-                let http = http.clone();
-                let leaderboard = leaderboard.clone();
-                
-                // reset first ge message lock
-                first_ge_sent.store(false, Ordering::SeqCst);
-
-                Box::pin(async move {
-                    let leaderboard = { leaderboard.lock().await };
-                        
-                    let leaderboard_top_10: String = 'top_10: {
-                        if leaderboard.len() == 0 {
-                            break 'top_10 "noone yet :(".to_string();
-                        }
-
-                        let mut sorted: Vec<(&u64, &u16)> = leaderboard.iter().collect();
-                        sorted.sort_by(|a, b| b.1.cmp(a.1));
-                        sorted.shrink_to(10);
-
-                        let mut top_10 = String::new();
-                        let mut position = 1;
-                        for (id, count) in sorted {
-                            let user_id = UserId::new(id.clone());
-                            let user = user_id.to_user(http.clone()).await.expect("couldnt get user from id for leaderboard");
-                            let username = user.global_name.unwrap();
-
-                            top_10.push_str(&format!("{}. {}: {}\n", position, username, count));
-
-                            position += 1;
-                        }
-                        top_10.remove(top_10.len() - 1);
-
-                        top_10
-                    };
-
-                    debug!("leaderboard retrieved:\n{leaderboard_top_10}");
-
-                    let embed = CreateEmbed::new()
-                        .colour(Colour::from_rgb(255, 0, 124))
-                        .title("good evening leaderboard")
-                        .description(leaderboard_top_10);
-                    let message = CreateMessage::new()
-                        .add_embed(embed);
-
-                    _ = channel.send_message(http, message).await;
-                })
-            }))
-            .build()
-            .unwrap();
-
-        sched.clone().add(job).await?;
+        let job = create_sunset_job(
+            http.clone(),
+            GENERAL_CHANNEL_ID,
+            sunset_time,
+            evening_bag.clone(),
+        )
+        .await;
+        {
+            *sunset_job_id.lock().unwrap() = sched.add(job).await?;
+        }
     }
 
     // refresh sunset job/time - 12:02am
@@ -120,14 +63,23 @@ pub async fn init_jobs(
                     let new_sunset_time = web::get_sunset_time().await.unwrap();
 
                     let id = { *sunset_job_id.lock().unwrap() };
-                    let job = create_sunset_job(http, GENERAL_CHANNEL_ID, new_sunset_time, evening_bag.clone()).await;
+                    let job = create_sunset_job(
+                        http,
+                        GENERAL_CHANNEL_ID,
+                        new_sunset_time,
+                        evening_bag.clone(),
+                    )
+                    .await;
 
                     _ = sched.remove(&id).await;
                     let new_id = sched.add(job).await.unwrap();
 
-                    { *sunset_time.lock().unwrap() = new_sunset_time; }
-                    { *sunset_job_id.lock().unwrap() = new_id; }
-
+                    {
+                        *sunset_time.lock().unwrap() = new_sunset_time;
+                    }
+                    {
+                        *sunset_job_id.lock().unwrap() = new_id;
+                    }
 
                     debug!("sunset job {id} replaced with {new_id}");
                 })
@@ -160,7 +112,7 @@ pub async fn init_jobs(
                 let motd = bag[rand_index];
                 bag.remove(rand_index);
 
-                let message = CreateMessage::new().content(motd);
+                let message = CreateMessage::new().content("Nothing ever happens.");
 
                 Box::pin(async move {
                     _ = channel.send_message(http, message).await;
@@ -179,7 +131,7 @@ async fn create_sunset_job(
     http: Arc<Http>,
     channel_id: u64,
     time: time::OffsetDateTime,
-    bag: Arc<Mutex<Vec<&'static str>>>
+    bag: Arc<Mutex<Vec<&'static str>>>,
 ) -> tokio_cron_scheduler::Job {
     let schedule = &format!("{} {} {} * * *", time.second(), time.minute(), time.hour())[..];
     let channel = serenity::ChannelId::new(channel_id);
@@ -203,11 +155,9 @@ async fn create_sunset_job(
             let motd = bag[rand_index];
             bag.remove(rand_index);
 
-            let main_message = CreateMessage::new().content(motd);
-            let ge_message = CreateMessage::new().content("good evening");
+            let ge_message = CreateMessage::new().content("Nothing ever happens.");
 
             Box::pin(async move {
-                _ = channel.send_message(&http, main_message).await;
                 _ = channel.send_message(http, ge_message).await;
             })
         }))

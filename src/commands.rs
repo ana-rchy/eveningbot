17e790d3f9e81fbb2a68bf128bb32c009768d643
ana_rchy <hmmmtesthmmm2@gmail.com>
2025-04-02 01:45:45 +0100

    no more temple of chudda :(

--- src/commands.rs
+++ src/commands.rs
@@ -11,7 +11,15 @@ type Context<'a> = poise::Context<'a, SharedData, Error>;
 
 #[poise::command(prefix_command, slash_command)]
 pub async fn fact_check(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.say("Nothing ever happens.").await?;
+    let (image, filename) = get_fact_check_image(ctx.data()).await;
+    let attachment = CreateAttachment::file(&image, filename).await.unwrap();
+
+    let reply = CreateReply {
+        attachments: vec![attachment],
+        ..Default::default()
+    };
+
+    ctx.send(reply).await?;
 
     Ok(())
 }
@@ -40,7 +48,27 @@ pub async fn roll(
     max_imaginary: Option<i64>,
     min_imaginary: Option<i64>,
 ) -> Result<(), Error> {
-    ctx.say("Nothing ever happens.").await?;
+    let min = min.unwrap_or(0);
+    let max = max.unwrap_or(100);
+
+    let rand_r = roll_2(min, max).await;
+
+    if min_imaginary.is_none() && max_imaginary.is_none() {
+        ctx.say(format!("{rand_r}")).await?;
+    } else {
+        let min_i = min_imaginary.unwrap_or(1);
+        let max_i = max_imaginary.unwrap_or(101);
+
+        let rand_i = roll_2(min_i, max_i).await;
+
+        ctx.say(format!("{rand_r}+{rand_i}i")).await?;
+    }
+
+    if rand_r == min {
+        ctx.say("CRIT FAIL. dumbass.").await?;
+    } else if rand_r == max {
+        ctx.say(format!("NAT {max} LETS GOOOOOOOOO")).await?;
+    }
 
     Ok(())
 }
@@ -60,14 +88,77 @@ async fn roll_2(mut min: i64, mut max: i64) -> i64 {
 
 #[poise::command(slash_command, prefix_command)]
 pub async fn uwuify(ctx: Context<'_>, text: Option<String>) -> Result<(), Error> {
-    ctx.say("Nothing ever happens.").await?;
+    if text.is_none() {
+        ctx.say("nu tewext unu ;(").await?;
+        return Ok(());
+    }
+
+    let uwuified = uwu_rs::Uwu::new()
+        .uwuify(text.unwrap())
+        .expect("uwufication failed bruh how tf");
+
+    ctx.say(uwuified).await?;
 
     Ok(())
 }
 
 #[poise::command(prefix_command)]
 pub async fn get_leaderboard(ctx: Context<'_>, param: Option<String>) -> Result<(), Error> {
-    ctx.say("Nothing ever happens.").await?;
+    let http = ctx.http();
+    let leaderboard = ctx.data().evening_leaderboard.lock().await;
+
+    let leaderboard_top_10: String = 'top_10: {
+        if leaderboard.len() == 0 {
+            break 'top_10 "noone yet :(".to_string();
+        }
+
+        let mut sorted: Vec<(&u64, &u16)> = leaderboard.iter().collect();
+        sorted.sort_by(|a, b| b.1.cmp(a.1));
+        sorted.shrink_to(10);
+
+        let mut top_10 = String::new();
+        let mut position = 1;
+        for (id, count) in sorted {
+            let user_id = UserId::new(id.clone());
+            let user = user_id
+                .to_user(http)
+                .await
+                .expect("couldnt get user from id for leaderboard");
+            let username = user.global_name.unwrap();
+
+            top_10.push_str(&format!("{}. {}: {}\n", position, username, count));
+
+            position += 1;
+        }
+        top_10.remove(top_10.len() - 1);
+
+        top_10
+    };
+
+    debug!("leaderboard retrieved by command:\n{leaderboard_top_10}");
+
+    let embed = CreateEmbed::new()
+        .colour(Colour::from_rgb(255, 0, 124))
+        .title("good evening leaderboard")
+        .description(leaderboard_top_10);
+
+    if let Some(param) = param {
+        if param == "general" {
+            const GENERAL_CHANNEL_ID: u64 = 1215048710074011692;
+            let channel = ChannelId::new(GENERAL_CHANNEL_ID);
+
+            let message = CreateMessage::new().add_embed(embed);
+
+            channel.send_message(http, message).await?;
+        }
+    } else {
+        let message = CreateReply {
+            embeds: vec![embed],
+            ..Default::default()
+        };
+
+        ctx.send(message).await?;
+    }
 
     Ok(())
 }
@@ -78,7 +169,24 @@ pub async fn get_leaderboard(ctx: Context<'_>, param: Option<String>) -> Result<
     required_bot_permissions = "BAN_MEMBERS"
 )]
 pub async fn roll_ban(ctx: Context<'_>) -> Result<(), Error> {
-    ctx.say("Nothing ever happens.").await?;
+    let roll = (rand::random::<u32>() % 1000000) + 1;
+    let roll_text = format!("roll: {roll}/1000000");
+
+    if roll == 1000000 {
+        let member = ctx.author_member().await.unwrap();
+        member
+            .ban_with_reason(ctx.http(), 0, "gg you won the lottery")
+            .await?;
+
+        let message = format!(
+            "{roll_text}\n{} has won the lottery and been BANISHED.",
+            member.user.name
+        );
+        ctx.say(message).await?;
+    } else {
+        let message = format!("{roll_text}\nno ban. keep gambling");
+        ctx.say(message).await?;
+    }
 
     Ok(())
 }
